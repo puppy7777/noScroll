79e14fd96a7661f686b836e73af517d84fecbea0
puppy7777 <whe12345678910@gmail.com>
2025-04-18 21:46:39 -0700

    me when i forgor the config saving

--- src/main/java/com/puppy7777/noscroll/NoScroll.java
+++ src/main/java/com/puppy7777/noscroll/NoScroll.java
@@ -4,18 +4,19 @@ import net.minecraft.init.Blocks;
 import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = NoScroll.MODID, version = NoScroll.VERSION)
 public class NoScroll {
     public static final String MODID = "noscroll";
     public static final String VERSION = "1.0";
 
-    /**
-     * If everything goes to plan, you should see this message in the console.
-     *
-     * This is the main class and method of your mod. It is the entry point for the mod and
-     * is used to register all the things that your mod needs to function.
-     */
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        System.out.println("[noScroll] Pre-initialization started");
+        configHandler.loadConfig(event.getSuggestedConfigurationFile());
+    }
+
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         System.out.println("ClientCommandHandler: " + ClientCommandHandler.instance);
